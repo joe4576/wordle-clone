75400bbe890165db398de320efe4d274a2f44941
Joe Brown <joebrown.4576@gmail.com>
2022-07-06 14:56:56 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <div>init</div>
+  </main>
+</template>
+
+<style>
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+</style>
